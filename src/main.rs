92d0f8c92656b9a09fb578722a264259f54fff3b
Geoffrey Parker <grp@gparker.co>
2015-07-28 17:47:51 -0500

    add iron and setup basic hello world webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, World!");
+    const PORT: i32 = 3000;
+
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "hello world")))
+    }).http("localhost:3000").unwrap();
 }
