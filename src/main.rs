bb2a6a366895dc6be7d61219359c90c013658489
Geoffrey Parker <grp@gparker.co>
2015-07-28 18:21:48 -0500

    add server start error handling and extract port to constant

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ use iron::prelude::*;
 use iron::status;
 
 fn main() {
-    const PORT: i32 = 3000;
+    const PORT: u16 = 3000;
 
-    Iron::new(|_: &mut Request| {
+    let server = Iron::new(|_: &mut Request| {
         Ok(Response::with((status::Ok, "hello world")))
-    }).http("localhost:3000").unwrap();
+    });
+
+    let server_started = server.http(("localhost", PORT));
+
+    match server_started {
+        Ok(_) => println!("Server started on port {}", PORT),
+        Err(e) => println!("Server failed to start with error: {}", e),
+    }
 }
