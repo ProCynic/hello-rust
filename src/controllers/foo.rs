313b423dfcb4d587189b1c7bbbe97f493e32f40a
Geoffrey Parker <grp@gparker.co>
2015-08-01 18:46:54 -0500

    an explicit type annotation prevents forgetting to unwrap result

--- src/controllers/foo.rs
+++ src/controllers/foo.rs
@@ -6,6 +6,6 @@ use super::super::models;
 use super::super::models::Model;
 
 pub fn get_foo(_: &mut Request) -> IronResult<Response> {
-    let u = models::User::find_one().unwrap();
+    let u: models::User = models::User::find_one().unwrap();
     Ok(Response::with((status::Ok, format!("here's a foo for user {}", u.id))))
 }
