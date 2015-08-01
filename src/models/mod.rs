4f3bc6dbf0b310d725a6368ca5c37310ece2b135
Geoffrey Parker <grp@gparker.co>
2015-08-01 15:33:31 -0500

    show how error handling would work

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,7 +1,11 @@
+extern crate postgres;
+
+use self::postgres::error::*;
+
 trait Model {
-    fn insert(&mut self);
-    fn update(&mut self);
-    fn find_one() -> Self;
+    fn insert(&mut self) -> Result<&Self, DbError>;
+    fn update(&mut self) -> Result<&Self, DbError>;
+    fn find_one() -> Result<Self, DbError>;
 }
 
 pub struct User {
@@ -9,17 +13,19 @@ pub struct User {
 }
 
 impl Model for User {
-    fn insert(&mut self) {
+    fn insert(&mut self) -> Result<&User, DbError>{
         // e.g. send an insert statement to db, update self with db generated id
         self.id = 1;
+        Ok(self)
     }
 
-    fn update(&mut self) {
+    fn update(&mut self) -> Result<&User, DbError>{
         // e.g. send an update statement and update a field based on what db returns
+        Ok(self)
     }
 
-    fn find_one() -> User {
+    fn find_one() -> Result<User, DbError> {
         // e.g. run a select statement on db and return only result
-        User {id: 1}
+        Ok(User {id: 1})
     }
 }
