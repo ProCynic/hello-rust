e51f09147d6bbbec99023ad833512942146ae19e
Geoffrey Parker <grp@gparker.co>
2015-08-01 18:32:07 -0500

    break user model into own file

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,35 +2,11 @@ extern crate postgres;
 
 use self::postgres::error::*;
 
-trait Model {
+pub trait Model {
     fn insert(&mut self) -> Result<&Self, DbError>;
     fn update(&mut self) -> Result<&Self, DbError>;
     fn find_one() -> Result<Self, DbError>;
     fn find() -> Result<Vec<Self>, DbError>;
 }
 
-pub struct User {
-    id: u64,
-}
-
-impl Model for User {
-    fn insert(&mut self) -> Result<&User, DbError>{
-        // e.g. send an insert statement to db, update self with db generated id
-        self.id = 1;
-        Ok(self)
-    }
-
-    fn update(&mut self) -> Result<&User, DbError>{
-        // e.g. send an update statement and update a field based on what db returns
-        Ok(self)
-    }
-
-    fn find_one() -> Result<User, DbError> {
-        // e.g. run a select statement on db and return only result
-        Ok(User {id: 1})
-    }
-
-    fn find() -> Result<Vec<User>, DbError> {
-        Ok(vec![User{id: 1}])
-    }
-}
+pub mod user;
---
+++ src/models/user.rs
@@ -0,0 +1,29 @@
+use models::Model;
+use super::postgres::error::*;
+
+pub struct User {
+    id: u64,
+}
+
+impl Model for User {
+    fn insert(&mut self) -> Result<&User, DbError>{
+        // e.g. send an insert statement to db, update self with db generated id
+        self.id = 1;
+        Ok(self)
+    }
+
+    fn update(&mut self) -> Result<&User, DbError>{
+        // e.g. send an update statement and update a field based on what db returns
+        Ok(self)
+    }
+
+    fn find_one() -> Result<User, DbError> {
+        // e.g. run a select statement on db and return only result
+        Ok(User {id: 1})
+    }
+
+    fn find() -> Result<Vec<User>, DbError> {
+        Ok(vec![User{id: 1}])
+    }
+
+}
