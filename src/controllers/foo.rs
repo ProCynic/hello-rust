eed111472f6df80049e935d124a8faa94f94aa01
Geoffrey Parker <grp@gparker.co>
2015-08-01 21:15:23 -0500

    explicit type

--- src/controllers/foo.rs
+++ src/controllers/foo.rs
@@ -8,6 +8,6 @@ use super::super::models::Model;
 pub fn get_foo(_: &mut Request) -> IronResult<Response> {
     let u: models::User = models::User::find_one().unwrap();
     // ok, looks like I can successfuly import a model and access it
-    let uid = match u.id {Some(id) => id.to_string(), None => "unknown".to_string()};
+    let uid: String = match u.id {Some(id) => id.to_string(), None => "unknown".to_string()};
     Ok(Response::with((status::Ok, format!("here's a foo for user {}", uid))))
 }
